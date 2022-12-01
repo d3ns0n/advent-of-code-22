4d7041d75c4101ead8e9b5c26154328e81e53e8a
D3ns0n <d3ns0n@d3ns0n.de>
2022-12-01 14:51:04 +0100

    add solution for day 1 - puzzle 1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # advent-of-code-22
 My participation of this years Advent of Code - https://adventofcode.com
+
+- [Day 1 - Part 1](./day-1/part-1/instructions.md)
+- 
---
+++ day-1/part-1/solution.kts
@@ -0,0 +1,16 @@
+import java.io.File
+
+var elfCounter = 1
+val caloriesPerElf = mutableMapOf<Int, Int>()
+
+File("input.txt").readLines().forEach {
+    if (it.isBlank()) {
+        elfCounter++
+    } else {
+        caloriesPerElf.put(elfCounter, caloriesPerElf.getOrDefault(elfCounter, 0) + it.toInt())
+    }
+}
+
+val (elf, calories) = caloriesPerElf.maxBy { it.value }
+
+print("Elf #$elf carries the most food with $calories calories.")
